a79fb795ebb567cf49e778e98d6c3a8665439517
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-10-13 19:29:02 +0530

    FIX : empty and Zero value input error fixed

--- script.js
+++ script.js
@@ -8,31 +8,35 @@ function onBtnClicked() {
   let initialPriceValue = Number.parseInt(initialPrice.value);
   let qtyStockValue = Number.parseInt(qtyStock.value);
   let currentPriceValue = Number.parseInt(currentPrice.value);
-  //   console.log(initialPriceValue);
-  //   console.log(qtyStockValue);
-  //   console.log(currentPriceValue);
+  console.log(initialPriceValue);
+  console.log(qtyStockValue);
+  console.log(currentPriceValue);
 
-  if (initialPriceValue > currentPriceValue) {
+  if (initialPriceValue == 0 || qtyStockValue == 0) {
+    resultToPrint = "please Enter valid input";
+  } else if (initialPriceValue > currentPriceValue) {
     console.log("if");
 
     var lossValue = initialPriceValue - currentPriceValue;
     var lossTotal = lossValue * qtyStockValue;
-    var lossPercentage = (lossValue / initialPriceValue) * 100;
+    var lossPercentage = (lossTotal / initialPriceValue) * 100;
     var resultToPrint = `Your Loss is ${lossTotal} & Loss Percent is ${lossPercentage}%`;
     // console.log(ResultValue);
   } else if (currentPriceValue > initialPriceValue) {
     console.log("elseif");
     var profitValue = currentPriceValue - initialPriceValue;
     var profitTotal = profitValue * qtyStockValue;
-    var profitPercentage = (profitValue / currentPriceValue) * 100;
+    var profitPercentage = (profitTotal / initialPriceValue) * 100;
 
     resultToPrint = `Your Profit is ${profitTotal} & Profit Percent is ${profitPercentage}%`;
     console.log(ResultValue);
+  } else if (initialPriceValue == currentPriceValue) {
+    resultToPrint = "Please Wait For the Market to Move";
   } else {
-    ResultValue = "Please Wait For the Market to Move";
+    resultToPrint = "please Enter valid input";
   }
-  //   console.log("outside", ResultValue);
   ResultValue.innerText = resultToPrint;
+  //   console.log("outside", ResultValue);
 }
 
 btnVlaue.addEventListener("click", onBtnClicked);
